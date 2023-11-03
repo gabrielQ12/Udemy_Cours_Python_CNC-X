6a1ce4f117febc5b313e201c297a83a366f07303
gabrielQ12 <gabriel.quinette1@gmail.com>
2023-11-03 19:36:16 +0100

    cours Partie 4

--- Inventory_Manager.py
+++ Inventory_Manager.py
@@ -26,6 +26,7 @@ class Main(QMainWindow, FORM_CLASS):
 
     def Handel_Buttons(self):
         self.refresh_btn.clicked.connect(self.GET_DATA)
+        self.search_btn.clicked.connect(self.SEARCH)
 
     def GET_DATA(self):
         db = sqlite3.connect("./sqlite/parts.db")
@@ -44,7 +45,56 @@ class Main(QMainWindow, FORM_CLASS):
                     row_number, column_number, QTableWidgetItem(str(data))
                 )
 
-        ## la le code
+        cursor2 = db.cursor()
+        cursor3 = db.cursor()
+
+        parts_nbr = """SELECT COUNT (DISTINCT PartName) FROM parts_table"""
+        ref_nbr = """SELECT COUNT (DISTINCT Reference) FROM parts_table"""
+
+        result_ref_nbr = cursor2.execute(ref_nbr)
+        result_parts_nbr = cursor3.execute(parts_nbr)
+
+        self.lbl_ref_nbr.setText(str(result_ref_nbr.fetchone()[0]))
+        self.lbl_parts_nbr.setText(str(result_parts_nbr.fetchone()[0]))
+        
+        cursor4=db.cursor()
+        cursor5=db.cursor()
+        
+        min_hole='''SELECT MIN(NumberOfHoles), Reference FROM parts_table'''
+        max_hole='''SELECT MAX(NumberOfHoles), Reference FROM parts_table'''
+        
+        result_min_hole=cursor4.execute(min_hole)
+        result_max_hole=cursor5.execute(max_hole)
+        
+        r1=result_min_hole.fetchone()
+        r2=result_max_hole.fetchone()
+        
+        self.lbl_min_hole.setText(str(r1[0]))
+        self.lbl_max_hole.setText(str(r2[0]))
+        self.lbl_min_hole_2.setText(str(r1[1]))
+        self.lbl_max_hole_2.setText(str(r2[1]))
+        
+        
+        
+
+    def SEARCH(self):
+        db = sqlite3.connect("./sqlite/parts.db")
+        cursor = db.cursor()
+
+        nbr = int(self.count_filter_txt.text())
+
+        command = """SELECT * FROM parts_table WHERE count<=?"""
+
+        result = cursor.execute(command, [nbr])
+
+        self.table.setRowCount(0)
+
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(
+                    row_number, column_number, QTableWidgetItem(str(data))
+                )
 
 
 def main():
